bbbdd9546d3a2027ffb83cc72619c83826cf0c05
jensjvh <43621451+jensjvh@users.noreply.github.com>
2024-06-24 00:36:36 +0300

    Add layout.html

--- routes.py
+++ routes.py
@@ -4,3 +4,11 @@ from flask import render_template
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
