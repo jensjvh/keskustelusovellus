ab133ccc6d6d38c837e44b5344860b331e8b0a7a
jensjvh <43621451+jensjvh@users.noreply.github.com>
2024-06-25 23:26:05 +0300

    Implement registration

--- routes.py
+++ routes.py
@@ -1,13 +1,24 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect
+
+import users
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/register")
+@app.route("/register", methods=["get", "post"])
 def register():
-    return render_template("register.html")
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+        if users.register(username, email, password):
+            return redirect("/")
+        else:
+            return render_template("register.html", message="Incorrect username or password.")
 
 @app.route("/login")
 def login():
