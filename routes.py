355f03f81f1c237c40ee9da0936aa5ebb3f43176
jensjvh <43621451+jensjvh@users.noreply.github.com>
2024-06-26 23:11:56 +0300

    Add login time tracking

--- routes.py
+++ routes.py
@@ -20,6 +20,14 @@ def register():
         else:
             return render_template("register.html", message="Incorrect username or password.")
 
-@app.route("/login")
+@app.route("/login", methods=["get", "post"])
 def login():
-    return render_template("login.html")
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if users.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("login.html", message="Incorrect username or password.")
