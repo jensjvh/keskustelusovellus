848a1e33e15552e6bc33eb1778df7a323880c0d5
jensjvh <43621451+jensjvh@users.noreply.github.com>
2024-11-21 20:19:31 +0200

    Display last reply time on the topics table

--- topics.py
+++ topics.py
@@ -5,7 +5,10 @@ from sqlalchemy import text
 
 def get_topics():
     sql = text("""
-        SELECT T.title AS title, COUNT(Th.id) AS threads, COUNT(R.id) AS replies
+        SELECT T.title AS title,
+               COUNT(Th.id) AS threads,
+               COUNT(R.id) AS replies,
+               COALESCE(MAX(R.created_time)::TEXT, 'No replies') AS last_reply
         FROM Topics T
         LEFT JOIN Threads Th ON T.id = Th.topic_id
         LEFT JOIN Replies R ON Th.id = R.thread_id
